b1a955729237e82a64a49dd57cf62507fe6f24a1
miss <miss@localhost.home>
2016-04-07 21:21:07 +0800

    添加一键安装脚本

---
+++ build.sh
@@ -0,0 +1,10 @@
+# 执行自定义命令
+sh ./alias/alias.sh
+
+# 使用 homebrew 安装各种软件
+sh ./homebrews/install.sh
+
+# 配置 sublime
+sublimeUserFord=~/Library/Application Support/Sublime Text 3/Packages/User
+rm -r $sublimeUserFord
+ln -s ./sublime/User $sublimeUserFord
