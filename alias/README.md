2f75382f2bc4b6c866f24b9e3f3a6f8ff5a55953
miss <miss@localhost.home>
2016-04-07 20:59:27 +0800

    添加自定义shell命令

---
+++ alias/README.md
@@ -0,0 +1,10 @@
+## 自定义 shell 命令
+
+## 命令列表
+
+```shell
+	
+	# 自定义mac os 中 ll 命令
+	alias ll="ls -al"
+	
+```
---
+++ alias/alias.sh
@@ -0,0 +1,7 @@
+
+#mac os中定义ll命令
+echo 'alias ll="ls -al"' >> ~/.bash_profile
+
+
+#使用bash_profile生效
+source ~/.bash_profile
